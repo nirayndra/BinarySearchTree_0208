62a72f9f84c2bd405a39b38ac558f6438632d908
nirayndra <faadiajasmine@gmail.com>
2026-06-09 11:10:01 +0700

    menambah void inorder

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -92,4 +92,20 @@ public:
         }
     }
 
+    void inorder(node *ptr)
+    {
+        if (root == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+
+        if(ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
+
 }
